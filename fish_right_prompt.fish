7139f057c26d1be209e2ddaeb203510ab15c5b92
Jaroslav Šmolík <grissius@gmail.com>
2019-11-19 18:20:42 +0100

    ✨ Add cwd on right prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,8 +1,21 @@
 function fish_right_prompt
+    set -l cwd
+
+    if git_is_repo
+        set root_folder (command git rev-parse --show-toplevel 2> /dev/null)
+        set parent_root_folder (dirname $root_folder)
+        set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+    else
+        set cwd (prompt_pwd)
+    end
+
+    echo -n -s $cwd
+
     set -l S (math $CMD_DURATION/1000)
     set -l M (math $S/60)
 
     set_color --dim
+    echo -n -s " " "⏱"
     if test $M -gt 1
         echo -n -s $M m
     else if test $S -gt 1

75face1186af73cd5079fc468f85148bdf3809c3
Jaroslav Šmolík <grissius@gmail.com>
2019-11-19 07:41:21 +0100

    ✨ Add execution time

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,3 +1,14 @@
 function fish_right_prompt
-  # Customize the right prompt
+    set -l S (math $CMD_DURATION/1000)
+    set -l M (math $S/60)
+
+    set_color --dim
+    if test $M -gt 1
+        echo -n -s $M m
+    else if test $S -gt 1
+        echo -n -s $S s
+    else
+        echo -n -s $CMD_DURATION ms
+    end
+    set_color normal
 end

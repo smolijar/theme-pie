515c866a4aa5993173d2f94a751d5269b96073b8
Jaroslav Šmolík <grissius@gmail.com>
2019-11-21 20:26:33 +0100

    ✨ Add git branch and wd/index status

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,8 +3,16 @@ function fish_prompt
 
   set -l symbol '> '
   set -l normal_color     (set_color normal)
+  set -l green_color     (set_color green)
+  set -l red_color     (set_color red)
   set -l success_color    (set_color $fish_color_success 2> /dev/null; or set_color cyan)
   set -l error_color      (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
+
+  if git_is_repo
+    git_is_staged && echo -n -s $green_color "⨯" $normal_color
+    git_is_dirty && echo -n -s $red_color "⨯" $normal_color
+    echo -n -s " " $primary_color "(" (git_branch_name) ")" $normal_color " "
+  end
 
   if test $last_command_status -eq 0
     echo -n -s $success_color $symbol $normal_color

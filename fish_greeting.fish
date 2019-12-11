03aec24fe6848fafbd3ec0ec5df9f0eef3b10fef
Jaroslav Šmolík <grissius@gmail.com>
2019-12-11 19:50:31 +0100

    ✨ Add rude cowsay greeting

--- fish_greeting.fish
+++ fish_greeting.fish
@@ -1,3 +1,16 @@
 function fish_greeting
-  # Customize fish greeting message
+    set -l say echo
+    if type -q cowsay
+        set -l cows
+        set -l available (cowsay -l | sed "1 d" | tr ' ' '\n')
+        set -l cool_cows "apt" "bud-frogs" "bunny" "duck" "head-in" "kitty" "koala" "luke-koala" "moofasa" "moose" "pony-smaller" "sodomized-sheep" "suse" "three-eyes" "unipony-smaller" "vader" "vader-koala"
+        for cow in $cool_cows
+            if string match -q $cow $available
+                set -a cows $cow
+            end
+        end
+        set say cowsay -f $cows[(math (date +%s) % (count $cows) + 1)]
+    end
+    set -l phrase (fortune /usr/share/fortune/off/toxic 2>/dev/null || fortune)
+    echo $phrase | $say
 end
